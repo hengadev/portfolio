9ba17e1677a9affe072ecac750c1373175b55582
GaryHY <henry.gary@hotmail.com>
2026-04-01 22:37:20 +0200

    feat(project): add Encx project page

---
+++ src/routes/(project)/project/encx/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+</script>
+
+<div>
+    Un point crucial du project Leviosa (link vers le project) qui me permet de
+    faciliter les etapes d'encryption, hashing de donnees.
+</div>
+
+<div>
+    Etant une fonctionnalite qui sera utilise regulierement, la difficulte etant
+    de developper une API qui ne soit pas un bottleneck.
+</div>
+
+<div>
+    Beaucoup appris, en computer science, sur le language de programmation go
+    (reflection, concurrency, memory model, the garbage collector)
+</div>
