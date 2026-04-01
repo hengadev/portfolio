85cc60ea07f3104f25d01dc8598ea03e91539b64
GaryHY <henry.gary@hotmail.com>
2026-04-01 22:56:28 +0200

    style: normalize indentation in hooks.server.ts

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,8 +1,8 @@
 import { redirect, type Handle } from "@sveltejs/kit";
 
 export const handle: Handle = async ({ event, resolve }) => {
-  if (event.url.pathname === "/soon") {
-    return resolve(event);
-  }
-  throw redirect(307, "/soon");
+    if (event.url.pathname === "/soon") {
+        return resolve(event);
+    }
+    throw redirect(307, "/soon");
 };
