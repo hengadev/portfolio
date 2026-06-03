2afb8a97ef76adf6db25e0ea718a07774dd6fb10
hengadev <henry.gary@hotmail.com>
2026-06-03 21:22:20 +0200

    feat: add experiments link to footer nav

--- src/lib/sections/Footer.svelte
+++ src/lib/sections/Footer.svelte
@@ -5,6 +5,7 @@
         { name: "Home", href: "/" },
         { name: "About", href: "/about" },
         { name: "Projects", href: "/projects" },
+        { name: "Experiments", href: "/experiments" },
         { name: "Blog", href: "/blog" },
         { name: "Contact", href: "/contact" },
     ];
