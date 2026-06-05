5074caf5cbedf165528a5e1b9da3b2c31799df7b
hengadev <henry.gary@hotmail.com>
2026-06-05 14:37:22 +0200

    feat(layout): add Nav and Footer to project and experiment detail pages

--- src/routes/(experiment)/experiments/+layout.svelte
+++ src/routes/(experiment)/experiments/+layout.svelte
@@ -1,9 +1,18 @@
+<script>
+    import Nav from "$lib/sections/Nav.svelte";
+    import Footer from "$lib/sections/Footer.svelte";
+</script>
+
+<Nav />
 <div class="content">
     <slot></slot>
 </div>
+<Footer />
 
 <style>
     .content {
-        margin-top: 0;
+        padding-top: 6rem;
+        padding-bottom: 1rem;
+        min-height: calc(100vh - 6rem);
     }
 </style>
--- src/routes/(project)/projects/+layout.svelte
+++ src/routes/(project)/projects/+layout.svelte
@@ -1,13 +1,18 @@
 <script>
-    // TODO: add the back button
+    import Nav from "$lib/sections/Nav.svelte";
+    import Footer from "$lib/sections/Footer.svelte";
 </script>
 
+<Nav />
 <div class="content">
     <slot></slot>
 </div>
+<Footer />
 
 <style>
     .content {
-        margin-top: 0;
+        padding-top: 6rem;
+        padding-bottom: 1rem;
+        min-height: calc(100vh - 6rem);
     }
 </style>
